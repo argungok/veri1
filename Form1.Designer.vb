3def10a8a198154a8ae1e3e3189a83d22dfb537d
argungok <140152@emu.edu.tr>
2021-04-06 11:47:39 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.PubIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CompanyNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PublishersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ContactsDataSet = New veri1.ContactsDataSet()
+        Me.PublishersTableAdapter = New veri1.ContactsDataSetTableAdapters.PublishersTableAdapter()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PublishersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ContactsDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.PubIDDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.CompanyNameDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.PublishersBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 36)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(379, 150)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'PubIDDataGridViewTextBoxColumn
+        '
+        Me.PubIDDataGridViewTextBoxColumn.DataPropertyName = "PubID"
+        Me.PubIDDataGridViewTextBoxColumn.HeaderText = "PubID"
+        Me.PubIDDataGridViewTextBoxColumn.Name = "PubIDDataGridViewTextBoxColumn"
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'CompanyNameDataGridViewTextBoxColumn
+        '
+        Me.CompanyNameDataGridViewTextBoxColumn.DataPropertyName = "Company Name"
+        Me.CompanyNameDataGridViewTextBoxColumn.HeaderText = "Company Name"
+        Me.CompanyNameDataGridViewTextBoxColumn.Name = "CompanyNameDataGridViewTextBoxColumn"
+        '
+        'PublishersBindingSource
+        '
+        Me.PublishersBindingSource.DataMember = "Publishers"
+        Me.PublishersBindingSource.DataSource = Me.ContactsDataSet
+        '
+        'ContactsDataSet
+        '
+        Me.ContactsDataSet.DataSetName = "ContactsDataSet"
+        Me.ContactsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'PublishersTableAdapter
+        '
+        Me.PublishersTableAdapter.ClearBeforeFill = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Kod yazmadan"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 205)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Kod yazarak"
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(12, 245)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(379, 150)
+        Me.DataGridView2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(410, 36)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(196, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Comboboxtan secilene göre bilgi getirme"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(413, 80)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(441, 159)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(527, 158)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(441, 205)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(24, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Fax"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(530, 204)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Label7"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(563, 80)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Bilgi Gönder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(672, 409)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PublishersBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ContactsDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ContactsDataSet As ContactsDataSet
+    Friend WithEvents PublishersBindingSource As BindingSource
+    Friend WithEvents PublishersTableAdapter As ContactsDataSetTableAdapters.PublishersTableAdapter
+    Friend WithEvents PubIDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CompanyNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class Form1
+    Dim cnn As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Casper\Desktop\dersler\btep210\uygulamalar\veri1\Contacts.mdf"";Integrated Security=True;Connect Timeout=30")
+    Dim cmd, cmd1 As New SqlCommand
+    Dim ds As New DataSet
+    Dim da As New SqlDataAdapter
+    Dim rdr As SqlDataReader
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If cnn.State = ConnectionState.Closed Then
+            cnn.Open()
+            cmd.Connection = cnn
+            cmd.CommandType = CommandType.Text
+            cmd.Parameters.Clear()
+            cmd.CommandText = "select Name,Fax from Publishers where PubID=@PubID"
+            cmd.Parameters.Add("@PubID", SqlDbType.Int).Value = ComboBox1.Text
+            rdr = cmd.ExecuteReader
+            If rdr.HasRows Then
+                While rdr.Read
+                    Label5.Text = rdr("Name")
+                    Label7.Text = rdr("Fax")
+                End While
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: Bu kod satırı 'ContactsDataSet.Publishers' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+        Me.PublishersTableAdapter.Fill(Me.ContactsDataSet.Publishers)
+
+
+        If cnn.State = ConnectionState.Closed Then
+            cnn.Open()
+
+            cmd.Connection = cnn
+            cmd.CommandText = CommandType.Text
+            cmd.CommandText = "select PubID,Name,Fax from Publishers"
+            da.SelectCommand = cmd
+            da.Fill(ds)
+            DataGridView2.DataSource = ds.Tables(0)
+
+            cmd1.Connection = cnn
+            cmd1.CommandText = "select PubID from Publishers"
+
+            rdr = cmd1.ExecuteReader()
+            If rdr.HasRows Then
+                While rdr.Read
+                    ComboBox1.Items.Add(rdr("PubID"))
+                End While
+            End If
+
+
+
+            cnn.Close()
+
+        End If
+    End Sub
+End Class
